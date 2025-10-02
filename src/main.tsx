cdee3b298d2a27208a5aee0ceb347c39d6cd6e42
Jorge <jorge.attie@meta.com.br>
2025-10-02 10:41:28 -0300

    deploy yml config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+//4jATuUjvA53FISrHbzfCA81r
+//prj_GBYr9TMGDPjFAKb93Lz1My458nGc
